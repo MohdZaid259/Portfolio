534a20ff40649012386ea5a611ec49eae2809955
Mohd Zaid <[razvizaid259@gmail.com]>
2024-06-13 12:28:45 +0530

    fixes skills

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,15 @@ import github from '../Media/github.png'
 import linkedin from '../Media/linkedin.png'
 import twitter from '../Media/twitter.png'
 import Typed from 'typed.js';
+import hero from '../Media/hero.png'
 
 function Home() {
     const element =useRef(null);
-  
+
     useEffect(() => {
       const typed = new Typed(element.current, {
         strings: ['Mohd Zaid.', 'a Web Developer.', 'a Web Designer.'],
-        typeSpeed: 80,
+        typeSpeed: 70,
       });
       return () => {
         typed.destroy();
@@ -20,20 +21,21 @@ function Home() {
     }, []);
 
   return (
-    <div className='bg-gradient-to-tr from-black via-gray-950 to-gray-800 text-white w-full h-full flex'>
-      <div className='flex flex-col justify-center pl-16'>
+    <div className='bg-gradient-to-tr from-black via-gray-950 to-gray-800 text-white w-full h-full flex justify-center'>
+      <div className='flex flex-1 flex-col justify-center pl-16'>
         <h3 className='text-2xl'>Hi, There!</h3>
         <h1 className='py-3 font-extrabold text-5xl'>I am <span ref={element} className="text-green-500">Mohd Zaid.</span></h1>
-        <p className='text-xl w-4/5'>A Creative Exlorer, on a compellong journey to reshape the future of Tech, guided by the thirst for innovation.</p>
-        <div className="flex gap-5 ml-7">
+        <p className='text-xl w-full'>A Creative Exlorer, on a compellong journey to reshape the future of Tech, guided by the thirst for innovation.</p>
+        <div className="flex gap-5">
             <Link to="https://x.com/zaidrazvi09" target='_blank'><img className='invert w-7 my-2 hover:bg-pink-500 p-1 rounded' src={twitter} alt="" /></Link>
             <Link to="https://www.linkedin.com/in/mohd-zaid-b28aa2248/" target='_blank'><img className='invert w-7 my-2 hover:bg-pink-500 p-1 rounded' src={linkedin} alt="" /></Link>
             <Link to="https://github.com/MohdZaid259" target='_blank'><img className='invert w-7 my-2 hover:bg-pink-500 p-1 rounded' src={github} alt="" /></Link>
         </div>
         <button className='my-2 bg-green-500 hover:bg-green-600 rounded-lg w-1/5 py-1 text-xl'>Download CV</button>
       </div>
-      <div>
-        <img src="" alt="" />
+      <div className='flex flex-1 items-center'>
+        {/* <div className=' h-3/4 w-auto' style={{ backgroundImage: `url(${hero})`}}></div> */}
+        <img className='pl-10 h-3/4 w-auto' src={hero} alt="" />
       </div>
     </div>
   )
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -19,70 +19,83 @@ import github from '../Media/skills/github.png'
 import '../style.css'
 
 function Skills() {
+  const img = [
+    html,css,atom,redux,tailwind,nodejs,appwrite,express,framer,js,cpp,java,c,mysql,mongodb,git,github
+  ]
+
   useEffect(()=>{
-    for(let i=0;i<25;i++){
+    for(let i=0;i<10;i++){
       const box = document.createElement('div')
       box.className='box absolute'
-
-      const size = Math.random() * (78 - 10) + 15;
+      const size = Math.random() * (30 - 10) + 15;
       box.style.width = `${size}px`
       box.style.height = `${size}px`
-      console.log(window.innerWidth/2)
       const randomX = Math.random() * (window.innerWidth/2 - size);
       const randomY = Math.random() * (window.innerHeight*4/5 - size);
       box.style.left = `${randomX}px`;
       box.style.top = `${randomY}px`;
       box.style.marginTop = '7rem'
       box.style.marginLeft = '3rem'
-
       shapeContainer.appendChild(box)
     }
   },[])
 
   return (
     <div className='h-full flex p-10 gap-5 bg-gradient-to-tr from-black  via-gray-950 to-gray-800 text-white'>
-      <div id='shapeContainer' className='flex gap-3 flex-wrap w-3/5'></div>
-      <div>
-        <div className='flex flex-col flex-wrap gap-5'>
-          <div className=''>
-            <div className='text-2xl font-semibold text-green-400'>Frontend Techstack</div>
-              <div className='ml-7 flex my-5 gap-5 flex-wrap'>
-                <img className='w-11' src={html}/>        
-                <img className='w-11' src={css}/>
-                <img className='w-11' src={atom}/>
-                <img className='w-11' src={redux}/>    
-                <img className='w-11' src={tailwind}/> 
-                <img className='w-11' src={framer}/>
-              </div>
-            <div className='text-2xl font-semibold text-green-400'>Backend Techstack</div>
-              <div className='ml-7 flex mt-5 gap-5 flex-wrap'>
-                <img className='w-11' src={nodejs}/>
-                <img className='invert w-11' src={express}/>
-                <img className='w-11' src={appwrite}/>
-              </div>
+      <div id='shapeContainer' className='flex gap-3 flex-wrap w-3/5'>
+      {img.map((item,id)=>{
+        return <>
+        <img key={id} className='p-2 border border-green-400 rounded-full w-16 h-16' src={item} alt="" />
+        </>
+      })}
+      </div>
+      <div className='flex flex-1'>
+        <div className='flex flex-col w-full flex-wrap gap-5'>
+          <div className='flex justify-around'>
+            <div>
+              <div className='text-2xl font-semibold -mx-3 text-green-400'>Frontend</div>
+                <div className='flex flex-col -mx-3'>
+                  <span className='text-lg'>HTML 5</span>        
+                  <span className='text-lg'>CSS 3</span>        
+                  <span className='text-lg'>ReactJs</span>        
+                  <span className='text-lg'>Redux</span>        
+                  <span className='text-lg'>Tailwind</span>        
+                  <span className='text-lg'>Framer</span>        
+                </div>
+            </div>
+            <div>
+              <div className='text-2xl font-semibold text-green-400'>Backend</div>
+                <div className='flex flex-col'>
+                  <span className='text-lg'>NodeJs</span>
+                  <span className='text-lg'>Express</span>
+                  <span className='text-lg'>Appwrite</span>
+                </div>
+            </div>
           </div>
+          <div className='mx-20 flex flex-col gap-5'>
           <div className=''>
             <span className='text-2xl font-semibold text-green-400'>Languages known</span>
-            <div className='ml-7 flex mt-5 gap-5 flex-wrap'>
-            <img className='w-11' src={js}/>
-            <img className='w-11' src={java}/>
-            <img className='w-11' src={c}/>
-            <img className='w-11' src={cpp}/>
+            <div className='flex flex-col'>
+              <span className='text-lg'>Javascript</span>
+              <span className='text-lg'>Python</span>
+              <span className='text-lg'>C</span>
+              <span className='text-lg'>C++</span>
             </div>
           </div>
           <div className=''>
             <span className='text-2xl font-semibold text-green-400'>Database Management</span>
-            <div className='ml-7 flex mt-5 gap-5 flex-wrap'>
-            <img className='w-11' src={mysql}/>
-            <img className='w-11' src={mongodb}/>
+            <div className='flex flex-col'>
+              <span className='text-lg'>MongoDb</span>
+              <span className='text-lg'>MySQL</span>
             </div>
           </div>
           <div className=''>
             <span className='text-2xl font-semibold text-green-400'>Version Control</span>
-            <div className='ml-7 flex mt-5 gap-5 flex-wrap'>
-            <img className='w-11 invert' src={github}/>
-            <img className='w-11' src={git}/>
+            <div className='flex flex-col'>
+              <span className='text-lg'>GitHub</span>
+              <span className='text-lg'>Git</span>
             </div>
+          </div>
           </div>
         </div>
       </div>
