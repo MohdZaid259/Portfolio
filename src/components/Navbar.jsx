4ae28ad8a69ed4051f5fa72e3a6a96be76a6e83a
Mohd Zaid <[razvizaid259@gmail.com]>
2024-07-06 14:33:20 +0530

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,12 +44,11 @@ export default function Header() {
                 <img className="w-7 mt-1 dark:invert rounded-sm" src={menu} alt="menu" />
               )}
             </div>
-            <div className="w-full h-auto">
-            <ul
+            <ul 
               onClick={handleHam}
               className={`dark:bg-inherit dark:text-white sm:text-base w-auto rounded-lg sm:rounded-none p-5 sm:p-0 absolute sm:static top-16 right-2 ${
                 ham
-                  ? "opacity-100 translate-y-0 backdrop-blur-sm"
+                  ? "opacity-100 translate-y-0 relative"
                   : "opacity-0 sm:opacity-100 -translate-y-[140%] sm:translate-y-0"
               } flex flex-col sm:flex-row items-end sm:items-center gap-3 sm:gap-4 sm:text-lg transition-all duration-200 ease-in-out`}
             >
@@ -57,7 +56,7 @@ export default function Header() {
               <a key={id} className='active:text-green-500 mx-3 hover:underline text-xl underline-offset-4' href={item.slug}>{item.name}</a>
             ))}
               <a href='#contact' className='active:text-green-500 mx-3 rounded-md dark:bg-green-600 text-white px-2 py-0.5 bg-pink-600 hover:bg-pink-700 dark:hover:bg-green-700 text-xl'>Contact Me</a>            
-            </ul></div>
+            </ul>
             <div className="ml-2 w-10 sm:w-12 invert dark:invert-0"><ThemeProvider><Switcher/></ThemeProvider></div>
           </div>
       </section>
